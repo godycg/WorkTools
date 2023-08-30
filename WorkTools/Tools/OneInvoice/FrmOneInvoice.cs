b9a90b179a5a03d9c8ac0ff67790caaf36bcedf6
godycg <godycg@163.com>
2023-08-30 17:46:43 +0800

    更新

--- WorkTools/Tools/OneInvoice/FrmOneInvoice.cs
+++ WorkTools/Tools/OneInvoice/FrmOneInvoice.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,11 @@ namespace WorkTools.Tools.OneInvoice {
     public partial class FrmOneInvoice : FrmBase {
         public FrmOneInvoice() {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            var frm = new Tools.FrmShowImage(Path.Combine(Application.StartupPath, "PrivateData", "20230822-164642.png"));
+            frm.ShowDialog();
         }
     }
 }
